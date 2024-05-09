2839dabb47f2bad4207cabba1a8b70c40fda3fdd
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-09 23:23:18 +0530

    vite react tailwind install

---
+++ tailwindreact2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 bg-green-400 border border-2 h-full w-full lg:text-gray-700 md:text-blue-700 md:bg-black sm:text-yellow-500">
+        Hi
+      </h1>
+
+      <h1 className="text-red-500 bg-green-400 border border-2 h-full w-full">
+        I
+      </h1>
+
+      <h1 className="text-red-500 bg-green-400 border border-2 h-full w-full">
+        Hi
+      </h1>
+      <h1 className="text-red-500 bg-green-400 border border-2 h-full w-full">
+        Hi
+      </h1>
+    </>
+  );
+}
+
+export default App;
