40c1b66c873a9f3a0616ad983e952ff804a42fa8
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-09 23:33:43 +0530

    tailwind install in react

---
+++ tailwindreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl font-bold underline text-yellow-400">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
